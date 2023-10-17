d02669cea2da921dc83bc40890b5a3b5367652e4
Rodrigo Sturm <rodristurm@gmail.com>
2023-10-17 00:07:28 -0300

    Agregue funcion para mostrar ocultar contraseña

---
+++ Software/CODIGO_Pagina_Web_Rodri/Log_In/script.js
@@ -0,0 +1,9 @@
+function showHide(){
+    var x = document.getElementById("password");
+    if(x.type === "password"){
+        x.type = "text";
+    }
+    else {
+        x.type = "password";
+    }
+}
