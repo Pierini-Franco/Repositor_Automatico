8611188b7f2f32a6154fd0928ba8530875cfa8ce
Rodrigo Sturm <rodristurm@gmail.com>
2023-10-19 15:10:20 -0300

    Funciones para que el usuario complete el formulario y funciones casillas esteticas

--- Software/CODIGO_Pagina_Web_Rodri/Sing_Up/script.js
+++ Software/CODIGO_Pagina_Web_Rodri/Sing_Up/script.js
@@ -11,24 +11,64 @@ function showHide(){
     }
 }
 
-var bool;
-
 function checkPassword(){
-    var x = document.getElementById("password");
-    var y = document.getElementById("re_password");
-    var g = document.getElementById("dot");
-    if(x.value === y.value){
-        g.style="position: absolute; left: 0; width: 10px; height: 10px; border-radius: 50%; background-color: #00a800";
-        bool = true;
+    var bool_password;
+    var password = document.getElementById("password");
+    var re_password = document.getElementById("re_password");
+    var dot = document.getElementById("dot");
+
+    if(!((re_password && re_password.value)) && !((password && password.value))){
+        dot.style="position: absolute; left: 0; width: 10px; height: 10px; border-radius: 50%; background-color: #7a7a7a";
+        bool_password = false;
+    }
+    if(password.value === re_password.value && ((re_password && re_password.value)) && ((password && password.value))){
+        dot.style="position: absolute; left: 0; width: 10px; height: 10px; border-radius: 50%; background-color: #00a800";
+        bool_password = true;
     }
     else{
-        g.style="position: absolute; left: 0; width: 10px; height: 10px; border-radius: 50%; background-color: #b91d08";
-        bool = false;
+        dot.style="position: absolute; left: 0; width: 10px; height: 10px; border-radius: 50%; background-color: #b91d08";
+        bool_password = false;
     }
+    return bool_password;
+}
+
+function checkApplication(){
+    var bool_userdata;
+    var name = document.getElementById("name");
+    var lastname = document.getElementById("lastname");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var re_password = document.getElementById("re_password");
+
+    if (!(name && name.value)) {
+        name.style="border: 1px solid #b91d08;"
+        bool_userdata = false
+    }
+    if (!(lastname && lastname.value)) {
+        lastname.style="border: 1px solid #b91d08;"
+        bool_userdata = false
+    }
+    if (!(email && email.value)) {
+        email.style="border: 1px solid #b91d08;"
+        bool_userdata = false
+    }
+    if(!(password && password.value)){
+        password.style="border: 1px solid #b91d08;"
+        bool_userdata = false
+    }
+    if(!(re_password && re_password.value)){
+        re_password.style="border: 1px solid #b91d08; margin-bottom: 15px"
+        bool_userdata = false
+    }
+    else{
+        bool_userdata = true;
+    }
+    return bool_userdata;
 }
 
 function checkLogin(){
-    if(bool === false){
+
+    if(checkApplication() === false || checkPassword() === false){
         alert("Error: Completar correctamente las casillas");
     }
     else{
@@ -36,4 +76,26 @@ function checkLogin(){
         var x = document.getElementById("password").value;
         console.log(x)
     }
+}
+
+function resetStyleName(){
+    var name = document.getElementById("name");
+    name.style="border-color: #7a7a7a";
+}
+
+function resetStyleLastname(){
+    var lastname = document.getElementById("lastname");
+    lastname.style="border-color: #7a7a7a";
+}
+
+function resetStyleEmail(){
+    var email = document.getElementById("email");
+    email.style="border-color: #7a7a7a";
+}
+
+function resetStylePasswords(){
+    var password = document.getElementById("password");
+    var re_password = document.getElementById("re_password");
+    password.style="border-color: #7a7a7a";
+    re_password.style="border-color: #7a7a7a; margin-bottom: 15px;";
 }
