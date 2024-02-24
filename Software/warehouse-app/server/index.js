80d5f1dda412de9aeb1e13888df57ad71ce0180c
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-24 15:44:30 -0300

    Update: Agregue evento conexion de usuarios, recuperar username de url y generar uuid

--- Software/warehouse-app/server/index.js
+++ Software/warehouse-app/server/index.js
@@ -1,9 +1,26 @@
 const http = require('http')
+const url = require('url')
+const uuidv4 = require("uuid").v4
 const { WebSocketServer } = require('ws')
 
 const server = http.createServer()
 const wsServer = new WebSocketServer({ server })
 const PORT = 8000
+// dictionarios de usuarios (username y state) y conexiones
+const users = { }
+const connections = { }
+
+// evento conexion a ws server
+wsServer.on("connection", (connection, request) =>{
+  // recuperar username de la url
+  const { username } = url.parse(request.url, true).query
+  const uuid = uuidv4()
+
+  console.log(username)
+  console.log(uuid)
+
+  // users[uuid] = username
+})
 
 server.listen(PORT, () =>{
   console.log(`WebSocket server running on port ${PORT}`)
