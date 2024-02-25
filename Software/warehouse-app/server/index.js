bed11b7ed9a3935c0048d031da8e7a421a24b14e
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-25 00:34:35 -0300

    Update: Agregue libreria mqtt - server puede conectar a broker, subcribir a topico y mandar mensaje

--- Software/warehouse-app/server/index.js
+++ Software/warehouse-app/server/index.js
@@ -3,6 +3,19 @@ const url = require('url')
 const uuidv4 = require("uuid").v4
 const { WebSocketServer } = require('ws')
 
+const mqtt = require("mqtt")
+const { error } = require('console')
+const clientId = 'emqxnodejs' + Math.random().toString(16).substring(2, 8)
+const username = 'Rodri'
+const password = 'NodeJS'
+const client = mqtt.connect('mqtt://broker.emqx.io:1883', {
+  clientId,
+  username,
+  password,
+})
+const topic = 'posicion/lugar'
+const qos = 0
+
 const server = http.createServer()
 const wsServer = new WebSocketServer({ server })
 const PORT = 8000
@@ -17,7 +30,6 @@ const handleMessage = (bytes, uuid) => {
   user.products = message
   console.log(`${user.username} ordeno ${JSON.stringify(user.products)}`)
   // enviar mensaje al broker
-
 }
 
 const handleClose = (uuid) =>{
@@ -25,6 +37,19 @@ const handleClose = (uuid) =>{
   delete users[uuid]
   delete connections[uuid]
 }
+// ni bien se conecta al broker, se subcribe al topico y publica mensaje
+client.on("connect", () =>{
+  client.subscribe(topic, { qos }, (error) =>{
+    if(error){
+      console.log('Cannot subcribe to topic')
+    }
+  })
+  client.publish(topic, "Conectado", { qos }, (error) =>{
+    if(error){
+      console.log('Cannot send message to topic')
+    }
+  })
+})
 
 // evento conexion a ws server
 wsServer.on("connection", (connection, request) =>{
