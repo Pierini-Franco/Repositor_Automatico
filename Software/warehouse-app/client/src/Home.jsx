e82f2096bbe71e128c30cdc1866d1e9820eae517
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-25 16:01:50 -0300

    Cree componente Home

---
+++ Software/warehouse-app/client/src/Home.jsx
@@ -0,0 +1,22 @@
+import { ProductCard } from "./components/ProductCard.jsx";
+import { Header } from './components/Header.jsx';
+import { productsJson as initialProductsJson } from './mocks/productsJson.json'
+import { useFilter } from "./hooks/useFilter.js";
+import { Footer } from "./components/Footer.jsx";
+import { Cart } from "./components/Cart.jsx";
+import { CartProvider } from "./context/cart.jsx";
+// header - products - footer
+
+export function Home(){
+   const { filterProducts, filters } = useFilter()
+   const filteredProducts = filterProducts(initialProductsJson)
+   console.log(filters)
+   return(
+      <CartProvider>
+         <Header />
+         <Cart />
+         <ProductCard filteredProducts={filteredProducts} />
+         <Footer />
+      </CartProvider>
+   );
+};
