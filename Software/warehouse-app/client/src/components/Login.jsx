5d3ca005addc71360fd3eaa12b1f8e51860d5516
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-26 18:16:00 -0300

    Cree estilos del login

--- Software/warehouse-app/client/src/components/Login.jsx
+++ Software/warehouse-app/client/src/components/Login.jsx
@@ -1,23 +1,29 @@
 import { useState } from "react"
+import './Login.css'
 
 export function Login({ updateUsername }){
   const [username, setUsername] = useState("")
   return(
     <>
-      <h1>Login</h1>
-      <p>Ingresar nombre de usuario: </p>
-      <form onSubmit={(e) =>{
-        e.preventDefault()
-        updateUsername(username)
-      }}>
-        <input 
-          type="text"
-          placeholder="username"
-          value={username}
-          onChange={(e) =>{setUsername(e.target.value)}}
-        />
-        <input type="submit" />
-      </form>
+      <section className="loginCard">
+        
+        <strong className="titleLogin">Login</strong>
+        <span>Ingresar nombre de usuario: </span>
+        <form onSubmit={(e) =>{
+          e.preventDefault()
+          updateUsername(username)
+        }}>
+          <input 
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(e) =>{setUsername(e.target.value)}}
+          />
+          <input type="submit" />
+        </form>
+        
+      </section>
+      
     </>
   )
 }
