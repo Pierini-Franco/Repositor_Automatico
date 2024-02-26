e12824ab33e8d51bfe937d060f8d420124566f8e
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-26 17:26:34 -0300

    Update: Componente Cart elimina productos del carrito y agregue leyenda cuando no hay productos en el carrito

--- Software/warehouse-app/client/src/components/Cart.jsx
+++ Software/warehouse-app/client/src/components/Cart.jsx
@@ -6,7 +6,44 @@ import { useCart } from "../hooks/useCart.js"
 export function Cart(){
   const checkBoxId = useId()
   const inputQuantityId = useId()
-  const { cart } = useCart()
+  const { cart, clearCart } = useCart()
+
+  const isThereProducts = (cart) => {
+    if(cart.length === 0){
+      return(
+        <>
+          <hr className="prCart-hrDivision"/>
+          <span className="prCart-noProducts">
+            Aun no hay productos en el carrito 
+          </span>
+          <hr className="prCart-hrDivision"/>
+        </>
+      )
+    }
+    else{
+      return(
+        cart.map(cart => {
+          return(
+            <section key={cart.id} className="prCart-container">
+              <article className="prCart">
+                <img className="prCart-image" src={cart.image} alt={cart.name} />
+                <div className="prCart-info">
+                  <strong>{cart.name}</strong>
+                  <div className="prCart-info-quantity">
+                    <label htmlFor={cart.id}>
+                      <small>Cantidad:</small>
+                    </label>
+                    <input id={cart.id} type="number" defaultValue={cart.quantity} max={cart.stock}/>{/* product.quantity, product.stock */}
+                  </div>
+                </div>
+              </article>
+              <hr className="prCart-hrDivision"/>
+            </section>
+          )
+        })
+      )
+    }
+  }
   
   return(
     <>
@@ -16,33 +53,16 @@ export function Cart(){
       <input id={checkBoxId} type="checkbox" hidden/>
       <aside className="cart-container">
         <section className="productsCart-container">
-          
           {
-            cart.map(cart => {
-              return(
-                <section key={cart.id} className="prCart-container">
-                  <article className="prCart">
-                    <img className="prCart-image" src={cart.image} alt={cart.name} />
-                    <div className="prCart-info">
-                      <strong>{cart.name}</strong>
-                      <div className="prCart-info-quantity">
-                        <label htmlFor={cart.id}>
-                          <small>Cantidad:</small>
-                        </label>
-                        <input id={cart.id} type="number" defaultValue={cart.quantity} max={cart.stock}/>{/* product.quantity, product.stock */}
-                      </div>
-                    </div>
-                  </article>
-                  <hr className="prCart-hrDivision"/>
-                </section>
-              )
-            })
+            isThereProducts(cart)
           }
-
         </section>
 
         <section className="prCart-buttonsContainer">
-          <button className="buttonClearCart">
+          <button 
+            className="buttonClearCart"
+            onClick={clearCart} // clear cart; poner 'clearCart()' no funciona
+            >
             <ClearFromCartIcon />
           </button>
           <button className="buttonCheckCart">
